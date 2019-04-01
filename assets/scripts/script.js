109e247f51014423d60bb7746c354b9b9a1c8404
VLasic2001 <vedro.lasic@gmail.com>
2019-04-01 22:27:06 +0200

    Implemented option to create new post

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -85,7 +85,7 @@ function loginSuccesfull() {
               <img
                 src="assets/images/profile-picture.jpg"
                 class="profile-picture"
-                onclick="showDetails(this)"
+                onclick="showDetails(${user.id}, this)"
               />
               <div class="user-info">
                 <p class="user-name">${user.name}</p>
@@ -103,25 +103,7 @@ function loginSuccesfull() {
             </div>
             <div class="details-popup">
               <div class="details">
-                <a class="close" onclick="closePopup(this)"></a>
-                <h1>Name: ${user.name}</h1>
-                <br />
-                <p>Username: ${user.username}</p>
-                <p>Email: ${user.email}</p>
-                <p>Phone: ${user.phone}</p>
-                <p>Web: ${user.website}</p>
-                <br />
-                <h3>Address:</h3>
-                <p>Street: ${user.address.street}</p>
-                <p>City: ${user.address.city}</p>
-                <p>Zipcode: ${user.address.zipcode}</p>
-                <p>Lat: ${user.address.geo.lat}</p>
-                <p>Lng: ${user.address.geo.lng}</p>
-                <br />
-                <h3>Company:</h3>
-                <p>Name: ${user.company.name}</p>
-                <p>Phrase: ${user.company.catchPhrase}</p>
-                <p>Bs: ${user.company.bs}</p>
+
               </div>
             </div>
             <div class="posts-popup">
@@ -143,14 +125,18 @@ function closePopup(e) {
     document.body.classList.remove("stop-scrolling");
     document.body.style.paddingRight = "0";
     e.parentElement.parentElement.style.display = "none";
-  }
+    e.parentElement.parentElement.querySelector(".details").innerHTML = '';
+}
 
 function closePostsPopup(e) {
   document.body.classList.remove("stop-scrolling");
   document.body.style.paddingRight = "0";
   e.parentElement.parentElement.style.display = "none";
   e.parentElement.innerHTML = '';
-;
+}
+
+function closeNewPostPopup() {
+  document.querySelector(".new-post-popup").remove()
 }
 
 function showPosts(id, e) {
@@ -166,8 +152,99 @@ function showPosts(id, e) {
     )
 
     e.parentElement.parentElement.
-    querySelector(".posts").innerHTML += '<a class="close" onclick="closePostsPopup(this)"></a>';
+    querySelector(".posts").innerHTML += `<a class="close" onclick="closePostsPopup(this)"></a>
+    <button class="new-post-button" onclick="newPost(${id})">New Post</button>`;
     e.parentElement.parentElement.querySelector(".posts-popup").style.display = "block";
     document.body.classList.add("stop-scrolling");
     document.body.style.paddingRight = "17px";
+}
+
+function showDetails(id, e){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => e.parentElement.parentElement.querySelector(".details-popup")
+        .querySelector(".details").innerHTML += users.filter(user => user.id === id).map(user => `
+            <h1>Name: ${user.name}</h1>
+            <br />
+            <p>Username: ${user.username}</p>
+            <p>Email: ${user.email}</p>
+            <p>Phone: ${user.phone}</p>
+            <p>Web: ${user.website}</p>
+            <br />
+            <h3>Address:</h3>
+            <p>Street: ${user.address.street}</p>
+            <p>City: ${user.address.city}</p>
+            <p>Zipcode: ${user.address.zipcode}</p>
+            <p>Lat: ${user.address.geo.lat}</p>
+            <p>Lng: ${user.address.geo.lng}</p>
+            <br />
+            <h3>Company:</h3>
+            <p>Name: ${user.company.name}</p>
+            <p>Phrase: ${user.company.catchPhrase}</p>
+            <p>Bs: ${user.company.bs}</p>    
+        `))
+
+        e.parentElement.parentElement.
+        querySelector(".details").innerHTML += '<a class="close" onclick="closePostsPopup(this)"></a>';
+        e.parentElement.parentElement.querySelector(".details-popup").style.display = "block";
+        document.body.classList.add("stop-scrolling");
+        document.body.style.paddingRight = "17px";
+}
+
+function newPost(id){
+  document.querySelector("main").innerHTML += `
+  <div class="new-post-popup">
+  <div class="new-post">
+    <a class="close" onclick="closeNewPostPopup()"></a>      
+    <h1 class="title">New Post</h1>
+            <p>User ID: ${id}</p>
+            <form class="new-post-form" action="javascript:SubmitNewPost(${id})">
+          <div class="input-div">
+            Title:<br />
+            <input
+              type="text"
+              name="title-input"
+              placeholder="Title"
+              required
+              class="title-input"
+            />
+          </div>
+          <div class="input-div">
+            Body:<br />
+            <input
+              type="text"
+              name="body-input"
+              placeholder="Body"
+              required
+              class="body-input"
+            />
+          </div>
+          <input type="submit" value="Post" class="submit" />
+        </form>
+        </div>
+    </div>`;
+    document.body.classList.add("stop-scrolling");
+    document.body.style.paddingRight = "17px";
+}
+
+function SubmitNewPost(id, e){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: document.getElementsByName("title-input")[0].value,
+          body: document.getElementsByName("body-input")[0].value,
+          userId: id
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }) 
+      .then(response => response.json())
+      .then(json => {if(json.id == 101){
+        alert(`Post added successfully \n 
+        User ID: ${json.userId} \n
+        Title: ${json.title} \n
+        `);
+        closeNewPostPopup();
+    }})
 }
